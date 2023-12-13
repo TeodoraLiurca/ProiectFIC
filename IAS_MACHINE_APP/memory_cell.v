dbbaa86823db74fff9523295fff901fec2f4d03d
Grozav Klaus <klaus.grozav@student.upt.ro>
2023-12-13 20:00:12 +0200

    PF-9: register.v disable on high impendance

--- IAS_MACHINE_APP/memory_cell.v
+++ IAS_MACHINE_APP/memory_cell.v
@@ -3,14 +3,14 @@ module memory_cell(input in, set, output out);
   assign a = ~(in&set);
   assign b = ~(a&set);
   assign out = ~(a&c);
-  assign c = ~(b&o);
+  assign c = ~(b&out);
 endmodule
 
-module byte_memory_cell(input [7:0] in, input set, output [7:0] out);
+module byte_memory_cell #(parameter WIDTH = 16)(input [WIDTH-1:0] in, input set, output [WIDTH-1:0] out);
   
   generate
     genvar i;
-    for(i = 0 ; i < 8 ; i=i+1)begin
+    for(i = 0 ; i < WIDTH ; i=i+1)begin
       memory_cell this_memory_cell(.in(in[i]), .set(set), .out(out[i]));
     end
   endgenerate
